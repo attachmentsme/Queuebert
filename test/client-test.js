ef63c6ca7cc4452ebdfd0e085b95a6e5e78f7ca1
bcoe <bencoe@gmail.com>
2012-02-09 16:22:01 -0800

    Implemented send message.

--- test/client-test.js
+++ test/client-test.js
@@ -4,12 +4,24 @@ var equal = require('assert').equal,
 	
 exports.tests = {
 	'should override default sendRequest object with one passed in': function(finished, prefix) {
-		var client = new Client({
+		var client = new Client('client_identifier', {
 			sendRequest: function(params, callback) {
 				equal(true, true);
 				finished();
 			}
 		});
 		client.sendRequest(null, null);
+	},
+	
+	'send message should invoke sendRequest with the appropriate parameters': function(finished, prefix) {
+		var client = new Client('client_identifier', {
+			sendRequest: function(params, callback) {
+				equal(params.type, 'put', prefix + ' type was not put.');
+				equal(params.identifier, 'client_identifier', prefix + ' client identifier not set');
+				equal(params.body.foo, 'bar', prefix + ' foo was not equal to bar');
+				finished();
+			}
+		});
+		client.sendMessage({foo: 'bar'});
 	}
 }
