4024b58b7bed6c1ca4a21cc38cddac9a219e18b6
bcoe <bencoe@gmail.com>
2012-02-10 15:45:43 -0800

    Created a client for running in the context of the background script.

--- test/background-client-test.js
+++ test/background-client-test.js
@@ -4,8 +4,25 @@ var equal = require('assert').equal,
 	
 exports.tests = {
 	'background client should have methods copied over from client': function(finished, prefix) {
-		var backgroundClient = new BackgroundClient();
+		var backgroundClient = new BackgroundClient({start: false});
 		equal(typeof(backgroundClient.handleMessage), 'function', prefix + ' client not extended.');
 		finished();
+	},
+	
+	'calling send message with a background client should invoke handleMessage directly': function(finished, prefix) {
+		var backgroundClient = new BackgroundClient({
+			server: {
+				handleMessage: function(request, sender, callback) {
+					equal(sender.tab.id, 'background', prefix + ' tabId was not correct');
+					equal(request.body.foo, 'bar', prefix + ' foo was not equal to bar');
+					finished();
+				}
+			},
+			start: false
+		});
+		backgroundClient.sendMessage({
+			action: 'upload',
+			body: {foo: 'bar'}
+		});
 	}
 };
